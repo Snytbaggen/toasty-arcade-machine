3e8c997798ec28d8b8f32ce60a2361d021e1f9de
Daniel Häggmyr <daniel@haggmyr.se>
2026-02-23 17:30:50 +0100

    Update upload script

--- godot/upload.sh
+++ godot/upload.sh
@@ -4,8 +4,14 @@ if [ -z "$1" ]
     exit 1;
 fi
 
+echo "Stopping current build"
+ssh lisse@$1 "pkill -f toastmachine.ar"
+
 echo "Uploading data folder"
 scp -r data_Toast\ machine_linuxbsd_arm32 lisse@$1:/home/lisse
 
 echo "Uploading executable"
-scp toastmachine.arm32 lisse@$1:/home/lisse 
+scp toastmachine.arm32 lisse@$1:/home/lisse
+
+echo "Restarting toast machine"
+ssh lisse@$1 "sudo reboot" 
